b39d65f2e392b557fd561a6c34be38a307b1270f
Alex <64910930+AlexeiTarasau@users.noreply.github.com>
2023-09-05 22:31:23 +0300

    Add files via upload

---
+++ Lesson26_Python/average.py
@@ -0,0 +1,4 @@
+a = int(input('Enter number: '))
+b = int(input('Enter number: '))
+
+print(int((a+b)/2))
---
+++ Lesson26_Python/enter_word.py
@@ -0,0 +1,7 @@
+word = input('Enter word: ')
+
+if word:
+    first_letter = word[0]
+    print('First letter is: ' + first_letter)
+else:
+    print('You entered an empty word. Please enter a word.')
